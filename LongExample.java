e27b902ea1551535e5b6bec01710b7b88db48ad6
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-06-20 19:54:25 +0900

    Update LongExample.java

--- LongExample.java
+++ LongExample.java
@@ -4,9 +4,9 @@ public class LongExample {
 
 	public static void main(String[] args) {
 		long var1 = 10;
-		long var1 = 20L;
-		long var1 = 1000000000000;	//컴파일러는 int로 간주하기 때문에 에러 발
-		long var1 = 1000000000000L;
+		long var2 = 20L;
+		long var3 = 1000000000000;	//컴파일러는 int로 간주하기 때문에 에러 발
+		long var4 = 1000000000000L;
 		
 		System.out.println(var1);
 		System.out.println(var2);
