ab8158ab569275c3059624f668dd4e1576d7e01f
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-07-10 18:48:29 +0900

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int score = 93;
+		
+		if(score >= 90) {
+			System.out.println("A등급 입니다");
+		}
+		
+		if(score < 90) {
+			System.out.println("B등급 입니");
+		}
+
+	}
+
+}
