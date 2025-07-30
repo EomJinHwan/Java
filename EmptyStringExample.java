56381a4d288683d875dcac6f8f7b1512e962312a
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-07-30 19:44:08 +0900

    Add files via upload

---
+++ EmptyStringExample.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class EmptyStringExample {
+
+	public static void main(String[] args) {
+		String hobby = "";
+		if(hobby.equals("")) {
+			System.out.println("빈 문자열");
+		}
+
+	}
+
+}
