95cbdf84627242cbb627ef0441916158ea66ff80
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-08-12 19:20:37 +0900

    Add files via upload

---
+++ Car2.java
@@ -0,0 +1,10 @@
+package ch06;
+
+public class Car2 {
+	
+	//생성자 선언
+	Car2(String model, String color, int maxSpeed) {
+		
+	}
+
+}
---
+++ CarExample2.java
@@ -0,0 +1,10 @@
+package ch06;
+
+public class CarExample2 {
+
+	public static void main(String[] args) {
+		Car2 car = new Car2("그랜저", "검정", 250);
+
+	}
+
+}
