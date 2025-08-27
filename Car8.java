95c5214ef09201171424db144d9a592e773496b3
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-08-27 18:47:41 +0900

    Add files via upload

---
+++ Car8.java
@@ -0,0 +1,13 @@
+package ch06;
+
+import ch06.hankook.SnowTire;
+import ch06.kumho.AllSeasonTire;
+
+public class Car8 {
+	//필드 선언
+	ch06.hankook.Tire tire1 = new ch06.hankook.Tire();	//클래스 전체 이름 사용으로 import문 필요없
+	ch06.kumho.Tire tire2 = new ch06.kumho.Tire();	//클래스 전체 이름 사용으로 import문 필요없
+	SnowTire tire3 = new SnowTire();	//import문 필요
+	AllSeasonTire tire4 = new AllSeasonTire();	//import문 필요
+
+}
