6a9a3948c1c8443dac739ef1df22552caa32b6eb
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-08-27 19:13:38 +0900

    Add files via upload

---
+++ package1/B.java
@@ -0,0 +1,7 @@
+package ch06.package1;	//A.java와 동일 패키지
+
+public class B {
+
+	//필드 선언
+	A a;	//o A클래스 접근 가능 (필드로 선언 가능)
+}
---
+++ package2/C.java
@@ -0,0 +1,9 @@
+package ch06.package2;	//A.java 와 패키지 다름
+
+import ch06.package1.B;
+
+public class C {
+	//필드선언
+	A a;	//x A클래스 접근 불
+	B b;	//o import문 으로 접근 가
+}
