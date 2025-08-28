4eca0e6b2a7089f560008b43cd1d9ac569f7746d
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-08-28 17:55:27 +0900

    Add files via upload

---
+++ CarExample9.java
@@ -0,0 +1,24 @@
+package ch06;
+
+public class CarExample9 {
+	public static void main(String[] args) {
+		//객체 생성
+		Car9 car = new Car9();
+
+		//잘못된 속도 변경
+		car.setSpeed(-50);
+		System.out.println("현재 속도 : " + car.getSpeed());
+		
+		//올바른 속도 변경
+		car.setSpeed(60);
+		System.out.println("현재 속도 : " + car.getSpeed());
+		
+		//멈춤
+		if(!car.isStop()) {
+			car.setStop(true);
+		}
+		System.out.println("현재 속도 : " + car.getSpeed());
+
+	}
+
+}
