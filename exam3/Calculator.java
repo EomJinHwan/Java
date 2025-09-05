be961c94dac234dd8a9d55d22cea890f39959ad8
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-09-05 19:48:39 +0900

    Add files via upload

---
+++ exam3/Calculator.java
@@ -0,0 +1,10 @@
+package ch07.exam3;
+
+public class Calculator {
+	//메소드 선언
+	public double areaCircle(double r) {
+		System.out.println("Calculator 객체의 areaCricle() 실행");
+		return 3.14159 * r * r;
+	}
+
+}
---
+++ exam3/SuperAirplane.java
@@ -0,0 +1,21 @@
+package ch07.exam3;
+
+public class SuperAirplane extends Airplane{
+	//상수 선언
+	public static final int NORMAL = 1;
+	public static final int SUPERSONIC = 2;
+	
+	//상태 필드 선언
+	public int flyMode = NORMAL;
+	
+	//메소드 재정의
+	@Override
+	public void fly() {
+		if(flyMode == SUPERSONIC) {
+			System.out.println("초음속 비행");
+		} else {
+			super.fly();
+		}
+	}
+
+}
