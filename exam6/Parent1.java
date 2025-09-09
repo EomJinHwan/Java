974dec8111be4044507663dc9f0c52a148ee04ce
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-09-09 19:36:10 +0900

    Add files via upload

---
+++ exam6/Parent1.java
@@ -0,0 +1,17 @@
+package ch07.exam6;
+
+public class Parent1 {
+	//필드 선언
+	public String field1;
+	
+	//메소드 선언
+	public void method1() {
+		System.out.println("Parent method1");
+	}
+	
+	//메소드 선언
+	public void method2() {
+		System.out.println("Parent method2");
+	}
+
+}
