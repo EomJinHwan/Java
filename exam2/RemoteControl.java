61228f4087da1a8db73e9973dd49af93ae9eeb53
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2025-11-25 19:02:34 +0900

    Add files via upload

---
+++ exam2/RemoteControl.java
@@ -0,0 +1,7 @@
+package ch08.exam2;
+
+public interface RemoteControl {
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;	//상수 선언
+
+}
---
+++ exam2/RemoteControlExample.java
@@ -0,0 +1,8 @@
+package ch08.exam2;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		System.out.println("최대 볼륨 : " + RemoteControl.MAX_VOLUME);
+		System.out.println("최소 볼륨 : " + RemoteControl.MIN_VOLUME);
+	}
+}
