b2a7f938b04907771f529175806925a4b83104db
EomJinHwan <167515443+EomJinHwan@users.noreply.github.com>
2026-03-26 11:08:37 +0900

    Add files via upload

---
+++ exam6/RemoteControl.java
@@ -0,0 +1,8 @@
+package ch08.exam6;
+
+public interface RemoteControl {
+	// 추상 메서드
+	void turnOn();
+	void turnOff();
+
+}
---
+++ exam6/Searchable.java
@@ -0,0 +1,7 @@
+package ch08.exam6;
+
+public interface Searchable {
+	// 추상 메서드
+	void search(String url);
+
+}
